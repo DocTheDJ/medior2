cba2b29a898eebdbcd6a2cf69d1e9afb2f80a839
DocTHeDJ <tomas.vitula.2000@gmail.com>
2025-07-22 13:26:59 +0200

    markdown in the homepage

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,6 +4,12 @@ All notable changes to this component will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## [0.3.0] - 2025-07-22
+### Authors
+Tomas Vitula
+### Updated
+- Markdown in the Homepage, used as per documentation of markdown-to-jsx
+
 ## [0.2.0] - 2025-05-16
 ### Authors
 Vilém Lipold
---
+++ src/components/MainText/MainText.tsx
@@ -0,0 +1,63 @@
+'use client';
+// client needs to be used due to the links
+
+import { ReactElement } from 'react';
+import Markdown from 'markdown-to-jsx';
+
+// markdown text from the task assignment
+const markdownData = `
+## How to use this application
+
+You see navigation on this page. You can use it to navigate to other pages. Other pages are [Article list](./articles/1) only. You can use it to see articles of any user.
+
+We are using best map web application in the world. You can see it on [Mapy.cz](https://mapy.cz). You can use it to see where is user from.
+
+This page is build with [Next.js](https://nextjs.org/), [React](https://react.dev/) and [Typescript](https://www.typescriptlang.org/). You can use it to build your own application too.
+
+`;
+
+// interface to make link replacement constant easier
+interface ICustomLink {
+  children: ReactElement;
+  href: string
+  [key: string]: unknown;
+}
+
+// custom link replacement
+const CustomLink = ({ children, ...props }: ICustomLink): ReactElement => {
+  // function to be called when the link is clicked
+  const handleClick = (): boolean => {
+    window.location.href = props.href;
+    return true;
+  };
+
+  return (
+    <span
+      {...props}
+      role={'link'}
+      tabIndex={0}
+      style={{ color: 'green', cursor: 'pointer' }}
+      onClick={handleClick}
+      onKeyDown={(e): boolean => e.key === 'Enter' && handleClick()}
+    >
+      {children}
+    </span>
+  );
+};
+
+export function MainText(): ReactElement {
+  // as per markdown-to-jsx documentation, override the 'a' to 'span'
+  return (
+    <Markdown
+      options={{
+        overrides: {
+          a: {
+            component: CustomLink,
+          },
+        },
+      }}
+    >
+      {markdownData}
+    </Markdown>
+  );
+}
